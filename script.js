42e21d6236c9251796fee5eea8aaf33788630f30
Courtney Vonada <cvonada@sva.edu>
2021-04-14 15:52:00 -0400

    Yarn Color Mess

---
+++ script.js
@@ -0,0 +1,28 @@
+let swatch = document.querySelectorAll('.colorpicker'),
+    storecolor1 = '#000000';
+
+
+swatch.forEach(element => { console.log('element');
+    element.addEventListener('change', (event) => {
+    storecolor1 = event.target.value;
+    updateYarn(storecolor1);
+    });
+}); 
+
+function updateYarn(storecolor1){ 
+   const yarn = document.getElementById('ball');
+   yarn.style.fill = storecolor1;
+}
+
+const selectElement = document.querySelector('#color');
+
+selectElement.addEventListener('change', (event) => {
+
+ console.log(event.target.value); 
+});
+
+const swatch1 = document.querySelector('#color1');
+
+swatch1.addEventListener('change', (event) => {
+ console.log(event.target.value); 
+});
