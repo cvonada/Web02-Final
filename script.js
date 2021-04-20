3747e94d4f361cf4baaf09a078ec1dded32e0d99
Lauren <laurengibbonz@gmail.com>
2021-04-20 16:03:32 -0400

    store colors

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
-let swatch = document.querySelectorAll('.colorpicker'),
-    storecolor1 = '#000000';
+const swatch = document.querySelectorAll('.colorpicker'),
+    colorChoice = document.querySelector('#color-choice');
 
+let storecolor1 = '#000000',
+    storecolor2 = '#000000',
+    storecolor3 = '#000000',
+    storecolor4 = '#000000',
+    storecolor5 = '#000000',
+    storecolor6 = '#000000',
+    storecolor7 = '#000000',
+    storecolor8 = '#000000',
+    numColors = 1,
+    whatColor = 1,
+    whatBall,
+    pickers = '';
 
-swatch.forEach(element => { console.log('element');
-    element.addEventListener('change', (event) => {
-    storecolor1 = event.target.value;
-    updateYarn(storecolor1);
-    });
-}); 
+updateColorPickers(numColors);
 
-function updateYarn(storecolor1){ 
-   const yarn = document.getElementById('ball');
-   yarn.style.fill = storecolor1;
+//Colors dropdown
+colorChoice.addEventListener('change', function() {
+    numColors = event.target.value;
+    updateColorPickers(numColors);
+});
+
+//Only show as many color pickers as selected in colors dropdown
+function updateColorPickers(numColors) {
+    pickers = '';
+    for(let i = 0; i < numColors; i++) {
+        pickers += `<div id="color${i}"><input class='colorpicker' type="color" id="colorpicker" data-color="${i+1}" value="${eval('storecolor' + (i + 1))}"></div>`;
+    }
+    document.getElementById('color-contents').innerHTML = pickers;
+    updateYarnColor();
 }
 
-const selectElement = document.querySelector('#color');
+function updateYarnColor() {
+    document.querySelectorAll('.colorpicker').forEach(element => { 
+        $('.colorpicker').on('change', function() {
+            whatColor = $(this).data('color');
+            eval('storecolor' + whatColor + '= ' + '"' +event.target.value + '"');
+            // passColor = event.target.value;
+            updateYarn(whatColor, eval('storecolor' + whatColor));
+        });
+}); 
+}
 
-selectElement.addEventListener('change', (event) => {
+function updateYarn(whatColor, passColor) { 
+    whatBall = $('.ball').get(whatColor - 1);
+    $(whatBall).css({fill: passColor});
+}
 
- console.log(event.target.value); 
-});
+// const selectElement = document.querySelector('#color');
+// selectElement.addEventListener('change', (event) => {
+//  console.log(event.target.value); 
+// });
 
-const swatch1 = document.querySelector('#color1');
+// const swatch1 = document.querySelector('#color1');
 
-swatch1.addEventListener('change', (event) => {
- console.log(event.target.value); 
-});
+// swatch1.addEventListener('change', (event) => {
+//  console.log(event.target.value); 
+// });
 
